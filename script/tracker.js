f48fe14be08731658fe029e1fe09560798152032
nafisanowshad <nafisanowshadmithila@gmail.com>
2026-02-23 08:36:32 +0600

    total card number dynamically added and jobs buttons toggling added

---
+++ script/tracker.js
@@ -0,0 +1,47 @@
+let interviewList = [];
+let rejectedList = [];
+
+let total = document.getElementById("dashboard-total");
+let interviewCnt = document.getElementById("dashboard-interview");
+let rejectedCnt = document.getElementById("dashboard-rejected");
+
+const allBtn = document.getElementById("all-btn");
+const interviewBtn = document.getElementById("interview-btn");
+const rejectedBtn = document.getElementById("rejected-btn");
+
+const allCardSection = document.getElementById("allCards");
+// console.log(allCardSection.children.length);
+
+// interviewList.push({Title:'job 1'});
+
+const mainContainer = document.querySelector("main");
+// console.log(mainContainer);
+
+function calculateCnt() {
+  total.innerText = allCardSection.children.length;
+
+  interviewCnt.innerText = interviewList.length;
+  rejectedCnt.innerText = rejectedList.length;
+}
+calculateCnt();
+
+function toggling(id) {
+  //   console.log("click", id);
+  allBtn.classList.remove("bg-[#3B82F6]", "text-[#FFFFFF]");
+  interviewBtn.classList.remove("bg-[#3B82F6]", "text-[#FFFFFF]");
+  rejectedBtn.classList.remove("bg-[#3B82F6]", "text-[#FFFFFF]");
+
+  allBtn.classList.add("bg-base-100", "text-[#64748B]");
+  interviewBtn.classList.add("bg-base-100", "text-[#64748B]");
+  rejectedBtn.classList.add("bg-base-100", "text-[#64748B]");
+
+  //   console.log(id);
+  const selected = document.getElementById(id);
+  //   console.log(selected);
+
+  selected.classList.add("bg-[#3B82F6]", "text-[#FFFFFF]");
+}
+
+mainContainer.addEventListener("click", function (event) {
+    
+});
